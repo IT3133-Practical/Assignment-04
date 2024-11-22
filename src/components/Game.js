925ee02a4b047238f59c2924c6e587d46ea70f91
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2024-11-22 23:04:51 +0530

    edit games.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,18 @@
 import "../assets/css/GameStyles.css";
 
 export default function Game() {
+    const [targetAnimal, setTargetAnimal] = useState(null);
+  const [result, setResult] = useState("");
+
+  useEffect(() => {
+    getRandomAnimal();
+  }, []);
+
+  const getRandomAnimal = () => {
+    const randomIndex = Math.floor(Math.random() * animals.length);
+    setTargetAnimal(animals[randomIndex]);
+    setResult("");
+  };
 
   return (
     <div className="game-wrapper">
