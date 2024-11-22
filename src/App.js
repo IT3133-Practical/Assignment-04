c29127451f45e783f9b0263eb4164bed35125be1
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2024-11-22 23:02:36 +0530

    update App.js and create games.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Game from "./components/Game";
 
 function App() {
   return (
-    <h1></h1>
+    <Game/>
   );
 }
 
---
+++ src/components/Game.js
@@ -0,0 +1,14 @@
+import "../assets/css/GameStyles.css";
+
+export default function Game() {
+
+  return (
+    <div className="game-wrapper">
+      <div className="header">
+        <h1>ANIMAL MATCHING GAME</h1>
+      </div>
+    </div>
+  );
+};
+
+
